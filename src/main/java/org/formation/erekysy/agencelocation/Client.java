20d8e663a9a1945b02664a7af9f66c2bd0fe3ef0
hp <hp@DESKTOP-O2BNECN>
2022-10-03 10:45:37 +0200

    2eme

---
+++ src/main/java/org/formation/erekysy/agencelocation/Client.java
@@ -0,0 +1,31 @@
+package org.formation.erekysy.agencelocation;
+
+public class Client {
+	private int age;
+	private String nom,prenom;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public void louer() {
+		
+	}
+	public void rendre() {
+		
+	}
+}
